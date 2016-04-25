1c4449cbd8a785fbb90d7e470f40057d07b4382d
Ignacio Ortuzar <iortuzar@gmail.com>
2016-04-24 21:25:39 -0300

    mejoras pequeñas

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,18 +1,15 @@
 class SettingsController < ApplicationController
-before_action :set_user, only: [:index, :other]
+before_action :set_user, only: [:index, :bulk_update]
 
   def index
     settings = Setting.joins("LEFT JOIN user_settings ON settings.id = user_settings.setting_id")
       .where('user_settings.user_id = ? or user_settings.user_id is null', @user.id)
       .select('settings.id, user_settings.value, settings.name')
 
-    data = {"data":[{"id":"1","type":"settings","attributes":{"name":"Campo2","value":"1000"}},
-      {"id":"2","type":"settings","attributes":{"name":"Campo","value":"2000"}}]}
-
     render json: settings
   end
 
-  def other
+  def bulk_update
     data = params[:data]
     data.each do |s|
       set = UserSetting.where(user_id: @user.id).where(setting_id: data[s]['id']).take
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   resources :settings do
     collection do
-      post :other
+      post :bulk_update
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
